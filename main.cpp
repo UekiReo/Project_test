03a4f59913d161d72628f58d29457899f2712cc0
k022g0005@g.neec.ac.jp <k022g0005@g.neec.ac.jp>
2023-04-14 21:44:36 +0900

    完成品

--- main.cpp
+++ main.cpp
@@ -1,79 +1,171 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
 
-// ウィンドウプロシーシャ
+//ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-	// メッセージに応じてゲーム固有の処理を行う
+	//メッセージに応じてゲームの固有の処理を行う
 	switch (msg) {
-		// ウィンドウが破棄された
+		//ウィンドウが破棄された
 	case WM_DESTROY:
-		// OSに対して、アプリ終了を伝える
+		//OSに対して、アプリの終了を伝える
 		PostQuitMessage(0);
 		return 0;
 	}
-
-	// 標準のメッセージ処理を行う
+	//標準のメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-// クライアント領域のサイズ
-const int32_t kClientWidth = 1280;
-const int32_t kClientHeight = 720;
+std::wstring ConvertString(const std::string& str)
+{
+	if (str.empty())
+	{
+		return std::wstring();
+	}
 
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
 
-// Windowsアプリでのエントリーポイント(main関数)
+std::string ConvertString(const std::wstring& str)
+{
+	if (str.empty())
+	{
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0)
+	{
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	OutputDebugStringA("Hello,DirectX!!\n");
+
+	//ウィンドウサイズ
+	const int32_t kClientWidth = 1280;//横幅
+	const int32_t kClientHeight = 720;//縦幅
+
+	//ウィンドウクラスの設定
 	WNDCLASS wc{};
-	// ウィンドウプロシーシャ
-	wc.lpfnWndProc = WindowProc;
-	// ウィンドウクラス名(なんでも良い)
-	wc.lpszClassName = L"CG2WindowClass";
-	// インスタンスハンドル
-	wc.hInstance = GetModuleHandle(nullptr);
-	// カーソル
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.lpfnWndProc = WindowProc; //ウィンドウプロシージャを設定
+	wc.lpszClassName = L"DirectXGame";         //ウィンドウクラス名
+	wc.hInstance = GetModuleHandle(nullptr);   //ウィンドウハンドル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);  //カーソル指定
 
-	// ウィンドウクラスを登録
+	//ウィンドウクラスをOSに登録する
 	RegisterClass(&wc);
-
-	// ウィンドウサイズを表す構造体にクライアント領域を入れる
-	RECT wrc = { 0,0,kClientWidth,kClientWidth };
-
-	// クライアント領域を元に実際のサイズにwrcを変更してもらう
+	//ウィンドウサイズ｛ X座標 Y座標 横幅 縦幅 ｝
+	RECT wrc = { 0,0,kClientWidth,kClientHeight };
+	//自動でサイズを補正する
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-	// ウィンドウの生成
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName,      // 利用するクラス名
-		L"CG2",				   // タイトルバーの文字(なんでも可)
-		WS_OVERLAPPEDWINDOW,   // よく見るウィンドウスタイル
-		CW_USEDEFAULT,		   // 表示X座標
-		CW_USEDEFAULT,		   // 表示Y座標
-		wrc.right - wrc.left,  // ウィンドウ横幅
-		wrc.bottom - wrc.top,  // ウィンドウ縦幅
-		nullptr,			   // 親ウィンドウハンドル
-		nullptr,			   // メニューハンドル
-		wc.hInstance,		   // インスタンスハンドル
-		nullptr);			   // オプション
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(wc.lpszClassName,//クラス名
+		L"DirectXGame",       //タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,  //標準的なウィンドウスタイル
+		CW_USEDEFAULT,        //表示X座標 (OSに任せる)
+		CW_USEDEFAULT,        //表示Y座標 (OSに任せる)
+		wrc.right - wrc.left, //ウィンドウ横幅
+		wrc.bottom - wrc.top, //ウィンドウ縦幅
+		nullptr,              //親ウィンドウハンドル
+		nullptr,              //メニューハンドル
+		wc.hInstance,         //呼び出しアプリケーションハンドル
+		nullptr);	          //オプション
 
 	ShowWindow(hwnd, SW_SHOW);
 
+	//DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+	// HRESULTはWindow系のエラーコードであり
+	//関数が成功したかどうかをSUCCEEDEDマクロで判定出来る
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	//初期化の根本的な部分でエラーが出た場合はプログラムが間違っているか、どうにも出来ない場合が多いのでassertにしておく
+	assert(SUCCEEDED(hr));
+	//使用するアダプタ用の変数。最初にnullptrを入れておく
+	IDXGIAdapter4* useAdapter = nullptr;
+	//良い順にアダプタを頼む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i,
+		DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) !=
+		DXGI_ERROR_NOT_FOUND; ++i) {
+		//アダプターの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));//取得出来ないのは一大事
+		//ソフトウェアアダプタでなければ採用
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			//採用したアダプタの情報をログに出力。wstringの方なので注意
+			Log(ConvertString(std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
+			break;
+		}
+		useAdapter = nullptr;//ソフトウェアアダプタの場合は見なかった事にする
+	}
+	//適切なアダプタが見つからなかったので起動できない
+	assert(useAdapter != nullptr);
+
+	ID3D12Device* device = nullptr;
+	//機能レベルとログ出力用の文字列
+	D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+	};
+	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
+	//高い順に生成できるか試していく
+	for (size_t i = 0; i < _countof(featureLevels); ++i) {
+		//採用したアダプターでデバイスを生成
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+		//指定した機能レベルでデバイスが生成出来たかを確認
+		if (SUCCEEDED(hr)) {
+			//生成出来たのでログ出力を行ってループを抜ける
+			Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
+			break;
+		}
+	}
+	//デバイスの生成がうまくいかなかったので起動出来ない
+	assert(device != nullptr);
+	Log("Complete create D3D12Device!!!\n");//初期化完了のログをだす
+
 	MSG msg{};
-	// ウィンドウの×ボタンが押されるまでループ
+	//ウィンドウの×ボタンが押されるまでループ
 	while (msg.message != WM_QUIT) {
-		// windowにメッセージが来てたら最優先で処理させる
+		//Windowにメッセージが来てたら最優先で処理させる
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 		else {
-			// ゲームの処理
+			//ゲームの処理
 		}
 	}
+	////文字列を格納する
+	//std::string str0{ "STRING!!!" };
+	////整数を文字列にする
+	//std::string str1{ std::to_string(10) };
+	//
+	//Log(std::format("('Д'):{}\n", str0));
+	//Log(std::format("string:{}\n", str1));
 
-	// 出力ウィンドウへの文字入力
-	OutputDebugStringA("Hello,DirectX!\n");
+
 
 	return 0;
 }
-
